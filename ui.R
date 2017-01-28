e2eb142bd66cc661d83488e5f9d94ccbcbab40a9
Burak Bahar <shomenuchi@gmail.com>
2017-01-28 00:00:56 -0500

    Update ui.R

--- ui.R
+++ ui.R
@@ -16,10 +16,9 @@ dashboardPage(
                ),
       menuItem("Plots", tabName = "plots",
                icon = icon("line-chart", "fa-lg"),
-               menuSubItem("Bland-Altman Plot", tabName = "subitem1"),
                menuSubItem("Scatter Plot", tabName = "subitem2"),
-               menuSubItem("Estimated Systematic Bias Plot", tabName='subitem4'),
-               menuSubItem("Coefficient Plot", tabName = "subitem3")
+               menuSubItem("Coefficient Plot", tabName = "subitem3"),
+               menuSubItem("Bland-Altman Plot", tabName = "subitem1")
                ),
       menuItem("Statistics", tabName = "stats",
                icon = icon("users", "fa-lg")
@@ -89,12 +88,14 @@ dashboardPage(
                   selectInput('batype', h5('Bland-Altman Plot Type'), 
                               choices=list('0.5*(X+Y) vs. Y-X' = 3,
                                            'X vs. Y-X' = 1,
-                                           'X vs. (Y-X)/X' = 2,
-                                           '0.5*(X+Y) vs. (Y-X)/X' = 4,
                                            'rank(X) vs. Y-X' = 5,
-                                           'rank(X) vs. (Y-X)/X' = 6,
-                                           'sqrt(X*Y) vs. Y/X' = 7,
-                                           '0.5*(X+Y) vs. (Y-X) / (0.5*(X+Y))' = 8)
+                                           '0.5*(X+Y) vs. (Y-X)/X' = 4,                                       
+                                           'X vs. (Y-X)/X' = 2,
+                                           'rank(X) vs. (Y-X)/X' = 6
+#                                           ,
+#                                           'sqrt(X*Y) vs. Y/X' = 7,
+#                                           '0.5*(X+Y) vs. (Y-X) / (0.5*(X+Y))' = 8
+                                           )
                               )
                   )
       ),
@@ -109,33 +110,33 @@ dashboardPage(
                                            'Deming' = 'Deming',
                                            'Weighted Deming' = 'WDeming',
                                            'Passing-Bablok' = 'PaBa',
-                                           'Passing-Bablok Large Data' = 'PaBaLarge')
+                                           'Passing-Bablok Large Dataset' = 'PaBaLarge')
                               ),
                   fixedRow(
                     column(6,
                     selectInput('cimethod', h5('CI Method'), 
                                 choices=list('Analytical' = 'analytical',
                                              'Jackknife' = 'jackknife',
-                                             'Bootstrap' = 'bootstrap',
-                                             'Nested Bootstrap' = 'nestedbootstrap')
+                                             'Bootstrap' = 'bootstrap'
+                                             )
                                 )
-                           ),
+                           )
+                    ,
                     column(6, selectInput('metbootci',h5('Bootstrap CI Method'),
-                                          choices = list('Quantile'='quantile',
-                                                         'Student'='Student',
-                                                         'BCa'='BCa',
-                                                         'tBoot'='tBoot')
+                                          choices = list('BCa'='BCa',
+                                                         'Quantile'='quantile')
                                           )
                            )
                   ),
                   fixedRow(
                     column(6, selectInput('cormet',h5('Correlation Method'),
                                           choices = list('Pearson'='pearson',
-                                                         'Kendall'='kendall',
-                                                         'Spearman'='spearman')
+                                                         'Spearman'='spearman',
+                                                         'Kendall'='kendall'
+                                          )
                     )
                     ),
-                    column(6, numericInput('syx', h5('Sy/Sx'), value = 1)
+                    column(6, numericInput('syx', h5('Error Ratio'), value = 1)
                            )
                   ),
                   fixedRow(
